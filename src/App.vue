e0d6b39bf9e1ea5acb9e74d6afd1adf914bfdad3
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-11-01 12:57:35 +0100

    v-bind und v-on

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <div>
     Hi,{{ name_first }}.
     <ul>
-      <li>Berlin</li>
-      <li>Hamburg</li>
-      <li>Frankfurt</li>
-    </ul>
-    <ul>
-      <li v-for="(city, index) in cities" v-bind:key="city.id">
-        {{ ++index }}. {{ city.name }}
+      <li
+        v-for="(todo, index) in todos"
+        :key="todo.id"
+        :class="{ finishedTask: todo.finished }"
+      >
+        <input type="checkbox" :checked="todo.finished" />
+
+        <a href="'?id='+todo.id">show</a> {{ ++index }}. {{ todo.name }}
       </li>
     </ul>
-    <p v-for="n in 10" v-bind:key="n">{{ n }}</p>
+    <button @click="test">Test</button>
   </div>
 </template>
 
@@ -20,32 +21,25 @@ export default {
   name: "App",
   data: () => ({
     name_first: "Cecilia",
-    cities: [
-      { id: 1, name: "Berlin" },
-      { id: 2, name: "Hamburg" },
-      { id: 3, name: "Frankfurt" },
+    todos: [
+      { id: 1, finished: false, name: "Vue JS lernen" },
+      { id: 2, finished: false, name: "Schwarze Tonne rein holen" },
+      { id: 3, finished: false, name: "Staubsauger starten" },
+      { id: 4, finished: true, name: "Büro umbauen" },
     ],
   }),
-  computed: {
-    fullName() {
-      return this.name_first + " " + this.name_last;
-    },
-  },
 };
 </script>
 
 <style>
-.age {
-  border: 1px solid red;
-  margin: 5px;
-  padding: 10px;
+.finishedTask {
+  text-decoration: line-through;
 }
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
