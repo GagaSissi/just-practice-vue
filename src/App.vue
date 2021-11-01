82061d169a6e4801dcbb26defdeb40986bd75f9a
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-11-01 11:02:05 +0100

    first initial push

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <strong>{{ name_first }}</strong
+    >,
+    {{ age + " " + gender + "," }}
+    {{ name_last }}
+    <hr />
+    {{ fullName }}
+    <hr />
+    {{ `${name_first} ${name_last}, ist ${age} Jahre jung.` }}
+    <hr />
+    {{ age >= 18 ? "volljährig" : "nicht volljärig" }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    name_first: "Cecilia",
+    name_last: "Strüber",
+    age: 38,
+    gender: "female",
+  }),
+  computed: {
+    fullName() {
+      return this.name_first + " " + this.name_last;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
